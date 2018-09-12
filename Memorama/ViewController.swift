757d7a10dcb4534a907b381bdef5eb20743e1e18
tonyvazgar <luis.vazquezga@udlap.mx>
2018-09-12 18:25:47 -0500

    primer comit con inicializacion del proyecto para trabajar con el

---
+++ Memorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memorama
+//
+//  Created by Tony Vazgar on 9/12/18.
+//  Copyright © 2018 CLTP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
