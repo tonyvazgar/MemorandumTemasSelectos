447474f872de837402a97dfa580c10b131b8e15b
Victor Eduardo Pulido Contreras <victoredu96@hotmail.com>
2018-09-18 17:35:13 -0500

    "CAMBIOS"

--- Memorama/ViewController.swift
+++ Memorama/ViewController.swift
@@ -24,7 +24,8 @@ class ViewController: UIViewController {
     var score_P1 = 0
     var score_P2 = 0
     
-    var all_images = ["1", "2", "2", "1", "3", "3"]
+    
+    var all_images = ["1", "2", "2", "1", "3", "3", "4", "4"]
     var images = [String]()
     
     @IBOutlet weak var carta11: UIImageView!
@@ -80,18 +81,28 @@ class ViewController: UIViewController {
     func checkFinale() -> Bool {
         return carta11.isHidden && carta12.isHidden && carta13.isHidden
         && carta21.isHidden && carta22.isHidden && carta23.isHidden
-        && carta31.isHidden && carta32.isHidden && carta33.isHidden
+        /*&& carta31.isHidden && carta32.isHidden && carta33.isHidden
         && carta41.isHidden && carta42.isHidden && carta43.isHidden
         && carta51.isHidden && carta52.isHidden && carta53.isHidden
-        && carta61.isHidden && carta62.isHidden && carta63.isHidden
+        && carta61.isHidden && carta62.isHidden && carta63.isHidden*/
     }
     
     
     
     @IBAction func startGame(_ sender: UIButton) {
-        all_images = ["1", "2", "2", "1", "3", "3"]
+        turn_P1 = true
+        all_images = ["1", "2", "2", "1", "3", "3", "4", "4"]
         images = [String]()
-        print("START GAME")
+        for _ in 0..<all_images.count {
+            let rand = Int(arc4random_uniform(UInt32(all_images.count)))
+            images.append(all_images[rand])
+            all_images.remove(at: rand)
+        }
+        let alertController = UIAlertController(title: "START GAME", message:
+            "Memorandum", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default,handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
     }
     
     @IBAction func tapCarta(_ sender: UITapGestureRecognizer) {
@@ -101,11 +112,19 @@ class ViewController: UIViewController {
                 carta11.image = UIImage(named: images[0])
                 if tap_count == 1 && checkAreTheSame(prev_image: prev_str_img, current_image: images[0]){
                     print("good 1 pair")
+                    if turn_P1 == false {
+                        score_P2 += 1
+                    } else {
+                        score_P1 += 1
+                    }
+                    print("Score P2 \(score_P2)")
+                    print("Score P1 \(score_P1)")
                     current_image = carta11
                     wait(turn_images: false)
                     tap_count = 0
                 } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[0]){
                     print("change of player")
+                    turn_P1 = !turn_P1
                     current_image = carta11
                     wait(turn_images: true)
                     tap_count = 0
@@ -123,6 +142,7 @@ class ViewController: UIViewController {
                     tap_count = 0
                 } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[1]){
                     print("change of player")
+                    turn_P1 = !turn_P1
                     current_image = carta12
                     wait(turn_images: true)
                     tap_count = 0
@@ -140,6 +160,7 @@ class ViewController: UIViewController {
                     tap_count = 0
                 } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[2]){
                     print("change of player")
+                    turn_P1 = !turn_P1
                     current_image = carta13
                     wait(turn_images: true)
                     tap_count = 0
@@ -158,6 +179,7 @@ class ViewController: UIViewController {
                     tap_count = 0
                 } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[3]){
                     print("change of player")
+                    turn_P1 = !turn_P1
                     current_image = carta21
                     wait(turn_images: true)
                     tap_count = 0
@@ -176,6 +198,7 @@ class ViewController: UIViewController {
                     tap_count = 0
                 } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[4]){
                     print("change of player")
+                    turn_P1 = !turn_P1
                     current_image = carta22
                     wait(turn_images: true)
                     tap_count = 0
@@ -184,9 +207,43 @@ class ViewController: UIViewController {
                     prev_str_img = images[4]
                     tap_count += 1
                 }
+            } else if carta == carta23 {
+                carta23.image = UIImage(named: images[5])
+                if tap_count == 1 && checkAreTheSame(prev_image: prev_str_img, current_image: images[5]){
+                    print("good 1 pair")
+                    current_image = carta23
+                    wait(turn_images: false)
+                    tap_count = 0
+                } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_str_img, current_image: images[5]){
+                    print("change of player")
+                    turn_P1 = !turn_P1
+                    current_image = carta23
+                    wait(turn_images: true)
+                    tap_count = 0
+                } else {
+                    prev_image = carta23
+                    prev_str_img = images[5]
+                    tap_count += 1
+                }
             }
         }
     }
+    
+    /*if checkFinale(){
+    if score_P1 > score_P2 {
+    let alertController = UIAlertController(title: "Juego Terminado", message:
+    "El ganador es Player 1 con \(score_P1) puntos", preferredStyle: UIAlertControllerStyle.alert)
+    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+    
+    self.present(alertController, animated: true, completion: nil)
+    } else if score_P2 < score_P1 {
+    let alertController = UIAlertController(title: "Juego Terminado", message:
+    "El ganador es Player 2 con \(score_P2) puntos", preferredStyle: UIAlertControllerStyle.alert)
+    alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+    
+    self.present(alertController, animated: true, completion: nil)
+    }
+    }*/
     
     
     override func viewDidLoad() {
