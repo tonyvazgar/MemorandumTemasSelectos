e8e90ee9e15d8baf804891a9651c1227e1cd2124
Luis A. Flores <luis.a.floress@hotmail.com>
2018-09-24 12:47:11 -0500

    Bug Cheat Fixed
    After face up two cards, you can no longer change the player or continue yourself if it wasn't a pair

--- Memorama/ViewController.swift
+++ Memorama/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Memorama
 //
-//  Created by Tony Vazgar
+//  Created by Luis Antonio Vazquez Garcia
 //             Luis Alberto Flores Sanchez
 //             Victor Eduardo Pulido Contreras
 //             Carlos Eduardo Castelan Vazquez
@@ -21,15 +21,15 @@ class ViewController: UIViewController {
     
     var turn_P1 = false
     var tap_count = 0
-    var prev_image : UIImageView!
-    var current_image : UIImageView!
     var prev_int_img : Int!
+    var curr_int_img : Int!
     var score_P1 = 0
     var score_P2 = 0
     var image_view_array: Array<UIImageView> = Array()
     var image_array: Array<UIImage> = Array()
     var all_images = [String]()
     var images = [String]()
+    var is_turned = [Bool]()
     
     @IBOutlet weak var carta11: UIImageView!
     @IBOutlet weak var carta12: UIImageView!
@@ -62,8 +62,8 @@ class ViewController: UIViewController {
     
     func wait(turn_images: Bool) {
         let delay = 1
-        let curr_img = self.current_image!
-        let prev_img = self.prev_image!
+        let curr_img = self.curr_int_img!
+        let prev_img = self.prev_int_img!
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay), execute: {
             if turn_images {
                 self.turnImages(prev_image: prev_img, current_image: curr_img)
@@ -73,14 +73,16 @@ class ViewController: UIViewController {
         })
     }
     
-    func turnImages(prev_image: UIImageView, current_image: UIImageView) {
-        prev_image.image = UIImage(named: "Back")
-        current_image.image = UIImage(named: "Back")
-        
+    func turnImages(prev_image: Int, current_image: Int) {
+        image_view_array[prev_image].image = UIImage(named: "Back")
+        is_turned[prev_image] = false
+        image_view_array[current_image].image = UIImage(named: "Back")
+        is_turned[current_image] = false
     }
-    func disableImages(prev_image: UIImageView, current_image: UIImageView) {
-        prev_image.isHidden = true
-        current_image.isHidden = true
+    
+    func disableImages(prev_image: Int, current_image: Int) {
+        image_view_array[prev_image].isHidden = true
+        image_view_array[current_image].isHidden = true
     }
     
     func checkFinale() -> Bool {
@@ -94,12 +96,14 @@ class ViewController: UIViewController {
         all_images = "1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9".components(separatedBy: ",")
         images = [String]()
         image_array = [UIImage]()
+        is_turned = [Bool]()
         
         for _ in 0..<all_images.count {
             let rand = Int(arc4random_uniform(UInt32(all_images.count)))
             image_array.append(UIImage(named: all_images[rand])!)
             images.append(all_images[rand])
             all_images.remove(at: rand)
+            is_turned.append(false)
         }
         
         resetCards()
@@ -108,7 +112,7 @@ class ViewController: UIViewController {
         player1Label.isHidden = false
     }
     
-    func checkTap(card: UIImageView, num_card: Int) {
+    func checkTap(num_card: Int) {
         if tap_count == 1 && checkAreTheSame(prev_image: prev_int_img, current_image: num_card){
             if !turn_P1 {
                 score_P2 += 1
@@ -117,81 +121,97 @@ class ViewController: UIViewController {
             }
             scorePlayer1.text = String(score_P1)
             scorePlayer2.text = String(score_P2)
-            current_image = card
+            curr_int_img = num_card
             wait(turn_images: false)
             tap_count = 0
         } else if tap_count == 1 && !checkAreTheSame(prev_image: prev_int_img, current_image: num_card){
             turn_P1 = !turn_P1
             player1Label.isHidden = !turn_P1
             player2Label.isHidden = turn_P1
-            current_image = card
+            curr_int_img = num_card
             wait(turn_images: true)
             tap_count = 0
         } else {
-            prev_image = card
             prev_int_img = num_card
             tap_count += 1
         }
     }
     
     @IBAction func tapCarta(_ sender: UITapGestureRecognizer) {
-        //var x = UITapGestureRecognizer.
         if let carta = sender.view {
             switch(carta) {
-            case carta11:
+            case carta11 where !is_turned[0]:
                 carta11.image = image_array[0]
-                checkTap(card: carta11, num_card: 0)
-            case carta12:
+                is_turned[0] = true
+                checkTap(num_card: 0)
+            case carta12 where !is_turned[1]:
                 carta12.image = image_array[1]
-                checkTap(card: carta12, num_card: 1)
-            case carta13:
+                is_turned[1] = true
+                checkTap(num_card: 1)
+            case carta13 where !is_turned[2]:
                 carta13.image = image_array[2]
-                checkTap(card: carta13, num_card: 2)
-            case carta21:
+                is_turned[2] = true
+                checkTap(num_card: 2)
+            case carta21 where !is_turned[3]:
                 carta21.image = image_array[3]
-                checkTap(card: carta21, num_card: 3)
-            case carta22:
+                is_turned[3] = true
+                checkTap(num_card: 3)
+            case carta22 where !is_turned[4]:
                 carta22.image = image_array[4]
-                checkTap(card: carta22, num_card: 4)
-            case carta23:
+                is_turned[4] = true
+                checkTap(num_card: 4)
+            case carta23 where !is_turned[5]:
                 carta23.image = image_array[5]
-                checkTap(card: carta23, num_card: 5)
-            case carta31:
+                is_turned[5] = true
+                checkTap(num_card: 5)
+            case carta31 where !is_turned[6]:
                 carta31.image = image_array[6]
-                checkTap(card: carta31, num_card: 6)
-            case carta32:
+                is_turned[6] = true
+                checkTap(num_card: 6)
+            case carta32 where !is_turned[7]:
                 carta32.image = image_array[7]
-                checkTap(card: carta32, num_card: 7)
-            case carta33:
+                is_turned[7] = true
+                checkTap(num_card: 7)
+            case carta33 where !is_turned[8]:
                 carta33.image = image_array[8]
-                checkTap(card: carta33, num_card: 8)
-            case carta41:
+                is_turned[8] = true
+                checkTap(num_card: 8)
+            case carta41 where !is_turned[9]:
                 carta41.image = image_array[9]
-                checkTap(card: carta41, num_card: 9)
-            case carta42:
+                is_turned[9] = true
+                checkTap(num_card: 9)
+            case carta42 where !is_turned[10]:
                 carta42.image = image_array[10]
-                checkTap(card: carta42, num_card: 10)
-            case carta43:
+                is_turned[10] = true
+                checkTap(num_card: 10)
+            case carta43 where !is_turned[11]:
                 carta43.image = image_array[11]
-                checkTap(card: carta43, num_card: 11)
-            case carta51:
+                is_turned[11] = true
+                checkTap(num_card: 11)
+            case carta51 where !is_turned[12]:
                 carta51.image = image_array[12]
-                checkTap(card: carta51, num_card: 12)
-            case carta52:
+                is_turned[12] = true
+                checkTap(num_card: 12)
+            case carta52 where !is_turned[13]:
                 carta52.image = image_array[13]
-                checkTap(card: carta52, num_card: 13)
-            case carta53:
+                is_turned[13] = true
+                checkTap(num_card: 13)
+            case carta53 where !is_turned[14]:
                 carta53.image = image_array[14]
-                checkTap(card: carta53, num_card: 14)
-            case carta61:
+                is_turned[14] = true
+                checkTap(num_card: 14)
+            case carta61 where !is_turned[15]:
                 carta61.image = image_array[15]
-                checkTap(card: carta61, num_card: 15)
-            case carta62:
+                is_turned[15] = true
+                checkTap(num_card: 15)
+            case carta62 where !is_turned[16]:
                 carta62.image = image_array[16]
-                checkTap(card: carta62, num_card: 16)
-            case carta63:
+                is_turned[16] = true
+                checkTap(num_card: 16)
+            case carta63 where !is_turned[17]:
                 carta63.image = image_array[17]
-                checkTap(card: carta63, num_card: 17)
+                is_turned[17] = true
+                checkTap(num_card: 17)
             default:
                 break
             }
